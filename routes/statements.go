dd5e1bd876fa504561974724e7bc66a973926e43
Lucas Farias <lukf95@gmail.com>
2023-12-30 13:37:34 +0100

    Create statement in db

--- routes/statements.go
+++ routes/statements.go
@@ -2,23 +2,40 @@ package routes
 
 import (
 	"cloud.google.com/go/firestore"
+	"context"
 	"github.com/labstack/echo/v4"
+	"log"
 	"net/http"
+	"time"
 )
 
 // Statement represents the structure of our resource
 type Statement struct {
-	Text   string `json:"text"`
-	UserID string `json:"user_id"`
+	Text      string `json:"text"`
+	UserID    string `json:"user_id"`
+	CreatedAt time.Time
 }
 
 func HandleCreateStatement(client *firestore.Client) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		//ctx := context.Background()
+		ctx := context.Background()
+		location, err := time.LoadLocation("CET")
 
-		// code
+		var req Statement
+		if err := c.Bind(&req); err != nil {
+			log.Fatalf("Failed to bind request: %v", err)
+		}
 
-		return c.JSON(http.StatusCreated, nil)
+		newS, _, err := client.Collection("statements").Add(ctx, Statement{
+			Text:      req.Text,
+			UserID:    req.UserID,
+			CreatedAt: time.Now().In(location),
+		})
+		if err != nil {
+			log.Printf("An error has occurred: %s", err)
+		}
+
+		return c.JSON(http.StatusCreated, interface{}(newS))
 	}
 }
 
